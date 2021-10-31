a5723067e05c03e0737b8704c9eecb6a2f84f818
uchaiaye53 <uchhai53@gmail.com>
2021-10-31 17:48:15 +0600

    student form

--- src/components/students/AddStudent.jsx
+++ src/components/students/AddStudent.jsx
@@ -2,10 +2,58 @@ import React from 'react'
 
 function AddStudent() {
     return (
-        <div>
-            AddStudent
+        <div className="flex">
+        <div className="md:w-3/6 mx-auto p-8 w-full">
+    
+          <p className="mt-8 text-center text-3xl text-green-700 font-bold">Add Student</p>
+    
+          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    
+          <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstname">
+            First Name
+          </label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="firstname" type="text" placeholder="first-name"/>
+         </div>
+    
+         <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastname">
+            Last Name
+          </label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="lastname" type="text" placeholder="last-name"/>
         </div>
-    )
+    
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="registration">
+          Registration NO
+          </label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="registration" type="number" placeholder="registration number"/>
+        </div>
+    
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+          Email
+          </label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="email"/>
+        </div>
+    
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter a password"/>
+        </div>
+        
+        <div className="text-center">
+        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            Submit
+        </button>
+        </div>
+    
+          </form>
+        </div>
+        </div>
+      );
 }
 
 export default AddStudent
