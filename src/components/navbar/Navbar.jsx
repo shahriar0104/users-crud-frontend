c8e5f25bf797a18d2e753f8862f0ec1a8a46251f
Akib Uz Zaman <akib.zaman.04@gmail.com>
2021-11-02 17:25:47 +0600

    updated homepage and navbar design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import {RiLogoutCircleLine} from 'react-icons/ri'
+import {AiFillHome} from 'react-icons/ai'
 
 function Navbar() {
     const history = useHistory()
@@ -13,22 +15,22 @@ function Navbar() {
     }
 
     return (
-        <nav className="flex lg:flex-row md:flex-row sm:flex-row  flex-col sticky top-0 z-50 justify-between bg-gray-800">
-            <div className="flex lg:flex-row md:flex-row sm:flex-row flex-col">
+        <nav className="flex flex-row sticky top-0 z-50 justify-between bg-gray-800">
+            <div className="flex flex-row">
                 <div className="flex items-center text-xl text-yellow-50 mx-4">DSi Classroom</div>
-                <div className="flex items-center h-16"
+                <div className="flex items-center h-16 cursor-pointer"
                 onClick={handleGoToHome}>
-                    <button className="bg-green-300 px-8 mx-4 py-2 w-32 rounded-lg hover:bg-green-400">
-                        Home
-                    </button>
+                    <div className="bg-green-300 px-2 mx-4 py-2 rounded-md hover:bg-green-400">
+                        <AiFillHome size={32}/>
+                    </div>
                 </div>
             </div>
 
-            <div className="flex items-center h-16"
+            <div className="flex items-center h-16 cursor-pointer"
             onClick={handleLogout}>
-                <button className="bg-green-300 w-32 px-8 mx-4 py-2 rounded-lg hover:bg-green-400">
-                    Log Out
-                </button>
+                <div className="bg-red-400 px-2 mx-4 py-2 rounded-lg hover:bg-red-500">
+                    <RiLogoutCircleLine size={32}/>
+                </div>
             </div>
         </nav>
     )
