8855c69438877bf19b94a8bf40db9dd66854eaa6
Akib Uz Zaman <akib.zaman.04@gmail.com>
2021-11-03 14:40:36 +0600

    added homepage connection

--- src/components/posts/PostList.jsx
+++ src/components/posts/PostList.jsx
@@ -3,44 +3,44 @@ import { RiDeleteBinLine } from 'react-icons/ri'
 import { FaRegEdit } from 'react-icons/fa'
 import {MdOutlineRemoveRedEye} from 'react-icons/md'
 import {Link} from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import {splitTime} from '../../helper/helperMethods'
+//import { useSelector } from 'react-redux'
 
-function PostList() {
-    const posts = useSelector(state => state.postReducer.posts)
+function PostList({posts}) {
+    //const posts = useSelector(state => state.postReducer.posts)
+    console.log(posts)
 
     return (
         <>
             {
                 posts.length !== 0 && posts.map((post) => {
                     return(
-                        <div key={post.id} className="border m-2 p-2 bg-gray-100 rounded-lg">
+                        <div key={post._id} className="border m-2 p-2 bg-gray-100 rounded-lg">
                             <div className="flex flex-row justify-between">
-                                <div className="flex flex-row"> 
-                                    <div className="font-bold text-lg mr-2">{post.teacherName}</div>
-                                    <div className="text-xs font-light text-gray-500 mt-1.5">[{post.timestamp}]</div>
+                                <div className="flex flex-row flex-wrap"> 
+                                    <div className="font-bold text-lg mr-2">{post.owner}</div>
+                                    <div className="text-xs font-light text-gray-600 mt-1.5">[{splitTime(post.time)}]</div>
                                 </div>
                                 
                                 <div className="flex flex-row space-x-3 mt-1">
-                                    <Link 
-                                    key={post.id} 
+                                    <Link S
+                                    key={post._id} 
                                     to={{
-                                        pathname: `/postdetails/${post.id}`,
+                                        pathname: `/postdetails/${post._id}`,
                                         state: {post}
                                     }}>
                                         <MdOutlineRemoveRedEye
                                         className="cursor-pointer" size={25}
                                         /> 
                                     </Link>
-                                            
-                                    <FaRegEdit 
-                                    className="cursor-pointer" size={22}
-                                    />
+                                    
                                     <RiDeleteBinLine 
                                     className="cursor-pointer" size={22}
                                     />
                                 </div>
                             </div>
-                            <div className="pt-1">{post.description}</div>
+                            <div className="pt-1">{post.details}</div>
+                            <div className="text-sm text-right text-gray-500">Comments: {post.comments.length}</div>
                         </div>
                     )
                 
