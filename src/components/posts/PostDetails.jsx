13b74cf549b1df252289e3390690f545a691a677
jahid-044 <jahidruetian044@gmail.com>
2021-11-04 15:43:06 +0600

    Post Update function done.

--- src/components/posts/PostDetails.jsx
+++ src/components/posts/PostDetails.jsx
@@ -1,50 +1,27 @@
 import { useQuery, gql, useMutation } from "@apollo/client";
-import React from "react";
+import React, { useState } from "react";
 import { useHistory, useParams } from "react-router";
 import Navbar from "../navbar/Navbar";
+import { GET_POST, POST_COMMENT, DELETE_POST,UPDATE } from "../../Queries/query";
 
 function PostDetails() {
   const id_post = useParams().postid;
+  const [update, setUpdate] = useState(false);
+  const history = useHistory();
+
   const PostId = {
     _id: id_post,
   };
-  const history = useHistory();
-
-  const GET_POST = gql`
-    query getPost($PostId: PostId) {
-      getPostById(postId: $PostId) {
-        title
-        details
-        comments {
-          commentor
-          commentDetails
-        }
-      }
-    }
-  `;
-
-  const POST_COMMENT = gql`
-    mutation postComment($comment: CommentInput) {
-      createComment(commentInput: $comment) {
-        postId
-        commentor
-        commentDetails
-      }
-    }
-  `;
-
-  const DELETE_POST = gql`
-    mutation deletePost($PostId: PostId) {
-      deletePost(postId: $PostId)
-    }
-  `;
 
   const [postComment] = useMutation(POST_COMMENT, {
     refetchQueries: [GET_POST, "getPost"],
   });
 
-  const [postDelete, { DeleteLoading, DeleteError, DeleteData }] =
-    useMutation(DELETE_POST);
+  const [updateDetails] = useMutation(UPDATE, {
+    refetchQueries: [GET_POST, "getPost"],
+  });
+
+  const [postDelete] = useMutation(DELETE_POST);
 
   const { loading, error, data } = useQuery(GET_POST, {
     variables: { PostId },
@@ -54,11 +31,10 @@ function PostDetails() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
-  console.log(data.getPostById.comments);
-
   function addComment(event) {
     event.preventDefault();
     const comment = event.target.body.value;
+    event.target.reset();
     postComment({
       variables: {
         comment: {
@@ -68,7 +44,22 @@ function PostDetails() {
       },
     });
   }
-
+ 
+  function updatePost(event) {
+    event.preventDefault();
+    const updatedDetails=event.target.details.value;
+    updateDetails({
+      variables:{
+        postId:{
+          _id:id_post
+        },
+        update:{
+          details:updatedDetails
+        }
+      }
+    });
+    setUpdate(false);
+  }
   async function deletePost() {
     await postDelete({
       variables: {
@@ -101,17 +92,42 @@ function PostDetails() {
             </div>
 
             <div>
-              <p className="my-2 px-4">
-                {data && data.getPostById && data.getPostById.details}
-              </p>
+              {update ? (
+                <form id="details-form" onSubmit={(event)=>updatePost(event)}>
+                  <div className="w-full px-3 my-2">
+                    <textarea
+                      className="rounded border border-gray-400 leading-normal resize-none w-full h-40 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"
+                      name="details"
+                      required
+                    >{data && data.getPostById && data.getPostById.details}</textarea>
+                  </div>
+                </form>
+              ) : (
+                <p className="my-2 px-4">
+                  {data && data.getPostById && data.getPostById.details}
+                </p>
+              )}
               <div className="w-full flex items-start justify-end px-3">
+                {update ? (
+                  <div>
+                    <button
+                      type="submit"
+                      className=" bg-indigo-500 text-white font-medium py-1 px-4 border border-gray-400 rounded-lg 
+                      tracking-wide mr-1 hover:bg-indigo-400"
+                      form="details-form"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                ) : null}
                 <div className="-mr-1">
-                  <input
-                    type="submit"
+                  <button
                     className=" bg-indigo-500 text-white font-medium py-1 px-4 border border-gray-400 rounded-lg 
-                    tracking-wide mr-1 hover:bg-indigo-400"
-                    value="Update Post"
-                  />
+                      tracking-wide mr-1 hover:bg-indigo-400"
+                    onClick={() => setUpdate(true)}
+                  >
+                    Update Post
+                  </button>
                 </div>
               </div>
             </div>
@@ -125,7 +141,7 @@ function PostDetails() {
                   <h2 className="px-4 pt-3 pb-2 text-gray-800 text-lg">
                     Add a new comment
                   </h2>
-                  <div className="w-full md:w-full px-3 mb-1 -mt-1">
+                  <div className="w-full pl-2 mb-1 -mt-1">
                     <textarea
                       className="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white focus:placeholder-opacity-0"
                       name="body"
@@ -133,7 +149,7 @@ function PostDetails() {
                       required
                     ></textarea>
                   </div>
-                  <div className="w-full flex items-start justify-end px-3">
+                  <div className="w-full flex items-start justify-end">
                     <div className="-mr-1">
                       <input
                         type="submit"
