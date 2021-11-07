e03fa5ed7b38ece4337d00f61b485b93c3ea49a2
Akib Uz Zaman <akib.zaman.04@gmail.com>
2021-11-07 12:19:27 +0600

    fixed some issues

--- src/components/posts/CreatePost.jsx
+++ src/components/posts/CreatePost.jsx
@@ -2,8 +2,10 @@ import React, {useState} from 'react'
 import {gql, useMutation} from "@apollo/client"
 
 function CreatePost() {
-    const [title, setTitle] = useState('')
-    const [details, setDetails] = useState('')
+    const [inputs, setInputs] = useState({
+        title: '', 
+        description: '',
+    })
 
     const ADD_POST = gql`
         mutation addPost($post: PostInput) {
@@ -11,7 +13,6 @@ function CreatePost() {
                 title
                 owner
                 details
-                comments
                 time
             }
         }
@@ -19,24 +20,21 @@ function CreatePost() {
     const [addPost] = useMutation(ADD_POST)
 
     const handleChange = e => {
-        const {title, details} = e.target
+        const {name, value} = e.target
 
-        setTitle(title)
-        setDetails(details)
+        setInputs({
+            ...inputs,
+            [name]: value
+        })
     }
 
     function handleAddPost(e) {
-        e.preventDefault()
-
-        console.log(title, details)
-
         addPost({
           variables: {
             post: {
-              title: title,
+              title: inputs.title,
               owner: localStorage.getItem('username'),
-              details: details,
-              comments: [],
+              details: inputs.description,
               time: new Date(),
             },
           },
@@ -45,19 +43,25 @@ function CreatePost() {
 
     return (
         <>
-            <div className="p-2">
-                <input type="text" name="title" className="w-full mb-2 p-2 rounded-md" placeholder="Enter post title"
+            <form className="p-2" onSubmit={handleAddPost}>
+                <input type="text" 
+                name="title" 
+                className="w-full mb-2 p-2 rounded-md" 
+                placeholder="Enter post title"
                 onChange={handleChange}
+                value={inputs.title}
                 required></input>
-                <textarea className="w-full h-28 my-1 p-2 border rounded-lg" type="text" 
-                placeholder="Announce something to your student..." name="details" 
+                <textarea className="w-full h-28 my-1 p-2 border rounded-lg" 
+                type="text" 
+                placeholder="Announce something to your student..." 
+                name="description" 
                 onChange={handleChange}
+                value={inputs.description}
                 required></textarea>
-                <button className="bg-blue-300 px-8 py-2 mb-2 mt-0 rounded-lg float-right hover:bg-blue-400"
-                onClick={handleAddPost}>
+                <button className="bg-blue-300 px-8 py-2 mb-2 mt-0 rounded-lg float-right hover:bg-blue-400">
                     Add Post
                 </button>
-            </div> 
+            </form> 
         </>
     )
 }
