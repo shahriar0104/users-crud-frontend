184f4f8a0c87d9fdb30fecf65b3b013cbb513c8d
jahid-044 <jahidruetian044@gmail.com>
2021-11-08 11:24:20 +0600

    Update post button will be only available to the owner of the post.

--- src/components/users/StudentList.jsx
+++ src/components/users/StudentList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../navbar/Navbar';
-import {gql, useQuery} from "@apollo/client"
+import {gql, useMutation, useQuery} from "@apollo/client"
 import { useHistory } from 'react-router';
 
 function StudentList() {
@@ -9,25 +9,44 @@ function StudentList() {
   const STUDENT_LIST = gql`
     query getstudents{
       getAllStudent{
+          _id
           username
           email
       }
     }
   `
 
+  const DELETE= gql`
+   mutation DeleteUser($userId: UserId){
+     deleteUser(userId: $userId)
+ }
+  `
+  const [deleteUser]=useMutation(DELETE,{
+    refetchQueries:[STUDENT_LIST,"getstudents"]
+  });
   const {loading, error, data} = useQuery(STUDENT_LIST)
   if(loading) return 'loading'
   if(error) return error.message
 
   const students = data.getAllStudent
 
+  function DeleteStudent(event,user_id){
+    event.preventDefault();
+    deleteUser({
+      variables:{
+        userId:{
+          _id:user_id
+        }
+      }
+    });
+  }
   return (
     <div className="flex flex-col">
       <Navbar />
       <div className="bg-yellow-50 m-4 mx-auto md:w-2/3 lg:w-1/2 sm:w-11/12 rounded-xl">
         <p className="mt-8 mb-4 text-center text-3xl text-black font-bold"> Student List</p>
 
-        {students.length !==0 ? (students.map(({username, email}) => (
+        {students.length !==0 ? (students.map(({username, email, _id}) => (
           <div key={username} className="list w-full">
             <div style={{ display: 'flex', justifyContent: "space-between inline block" }}>
               <div className="w-full px-3 mx-5 my-1 border-white border-4 rounded-lg
@@ -42,7 +61,7 @@ function StudentList() {
                   <div>
                     <div className=' flex flex-row h-12'>
                       <button className="bg-green-50 transition transform hover:scale-110 px-4 py-0 my-2 mx-4 rounded-full "><i className="fas fa-pen"></i></button>
-                      <button className="bg-green-50 transition transform hover:scale-110 px-4 py-0 my-2 mx-4 rounded-full"><i className="fa fa-trash"></i></button>
+                      <button className="bg-green-50 transition transform hover:scale-110 px-4 py-0 my-2 mx-4 rounded-full" onClick={(event)=>DeleteStudent(event,_id)}><i className="fa fa-trash"></i></button>
                     </div>
                   </div>
                 }
